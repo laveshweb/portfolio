050d14c7140f52a8a5936740f41aa69f8c71cbbf
Lavesh Bhagwani <laveshthecoder@gmail.com>
2026-03-11 21:30:25 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './sections/Navbar'
 import Hero from './sections/Hero'
+import Project from './sections/Project'
 
 const App = () => {
   return (
     <div className= ' h-screen bg-green-500'>
       <Navbar/>
       <Hero/>
+      <Project/>
     </div>
   )
 }
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -7,8 +7,8 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="w-full  top-0 left-0 bg-white shadow-md  ">
-      <div className="max-w-7xl mx-auto px-8  py-[10px]  flex justify-between items-center">
+    <nav className="w-full  top-0 left-0 bg-white shadow-md   z-50 ">
+      <div className="max-w-7xl mx-auto px-4 lg:px-8  py-[10px]  flex justify-between items-center whitespace-nowrap">
 
         {/* Logo */}
         <div className="flex items-center">
@@ -16,7 +16,7 @@ const Navbar = () => {
             src={Logo}
             alt="Logo"
             className="h-11 md:h-12 lg:h-14 w-auto object-contain"
-          />  
+          />
         </div>
 
         {/* Desktop Menu */}
@@ -36,23 +36,24 @@ const Navbar = () => {
 
         {/* Mobile Icon */}
         <div
-  className="md:hidden text-[26px] max-[450px]:text-[30px] cursor-pointer"
-  onClick={() => setIsOpen(!isOpen)}
->
-  {isOpen ? (
-    <HiOutlineX className="text-gray-600" />
-  ) : (
-    <HiOutlineMenu className="text-gray-600" />
-  )}
-</div>
+          className="md:hidden text-[26px] max-[450px]:text-[30px] cursor-pointer "
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <HiOutlineX className="text-gray-600" />
+          ) : (
+            <HiOutlineMenu className="text-gray-600" />
+          )}
+        </div>
       </div>
 
       {/* Mobile Menu */}
       {/* Mobile Menu */}
       {/* Mobile Menu */}
       <div
-        className={`md:hidden fixed top-[65px] left-0 w-full h-[calc(110vh-80px)] bg-white flex flex-col  transition-all duration-800 ${isOpen ? "opacity-100 visible" : "opacity-0 invisible"
-          }`}
+        className={`md:hidden fixed top-[100px] left-0 w-full h-[calc(110vh-80px)] bg-white z-50
+        flex flex-col transition-all duration-800
+        ${isOpen ? "opacity-100 visible" : "opacity-0 invisible"}`}
       >
         <ul className="space-y-8 text-2xl font-[font1] text-gray-600 text-center mt-[20%]">
           {["Projects", "Experience", "Skills", "About Me", "Contact"].map(
